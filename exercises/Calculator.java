e781d1c2a0bc2901973f80ec6904e8e9444770b0
gianca1994 <gianca9405@gmail.com>
2021-03-17 19:28:28 -0600

    update

--- exercises/Calculator.java
+++ exercises/Calculator.java
@@ -13,24 +13,46 @@ public class Calculator {
         System.out.println("Second number:");
         int num2 = variable.nextInt();
 
-        public void sum() {
-            int resultSum = num1 + num2;
-            System.out.println("resultSum: " + resultSum);
-        }
+        System.out.println("Enter the option you want to perform: 
+            1- Sum
+            2- Subtraction
+            3- Multiplication
+            4- Division
+            5- Exit"
+        );
 
-        public void subtraction() {
-            int resultSum = num1 - num2;
-            System.out.println("resultSubtraction: " + resultSubtraction);
-        }
+        int operacion = variable.nextInt();
 
-        public void multiplication() {
-            int resultMultiplication = num1 * num2;
-            System.out.println("resultMultiplication: " + resultMultiplication);
-        }
+        switch(operacion){
+            case "1":
+                public void sum() {
+                    int resultSum = num1 + num2;
+                    System.out.println("resultSum: " + resultSum);
+                }
+                break;
 
-        public void division() {
-            float resultDivision = num1 / num2;
-            System.out.println("resultDivision: " + resultDivision);
-        }
+            case "2":
+                public void subtraction() {
+                    int resultSum = num1 - num2;
+                    System.out.println("resultSubtraction: " + resultSubtraction);
+                }
+                break;
+
+            case "3":
+                public void multiplication() {
+                    int resultMultiplication = num1 * num2;
+                    System.out.println("resultMultiplication: " + resultMultiplication);
+                }
+                break;
+            
+            case "4":
+                public void division() {
+                    float resultDivision = num1 / num2;
+                    System.out.println("resultDivision: " + resultDivision);
+                } 
+                break;
+
+            case "5":
+                break;
     }
 }
