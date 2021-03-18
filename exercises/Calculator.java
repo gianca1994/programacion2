f34dbf394619406b77653d4c851da3c1cfb893c8
Giancarlo <gianca9405@gmail.com>
2021-03-17 22:21:33 -0300

    update

--- exercises/Calculator.java
+++ exercises/Calculator.java
@@ -1,45 +1,31 @@
 import java.util.Scanner;
 
-import jdk.dynalink.beans.StaticClass;
-
-class Calculator {
-
+class Main {
     public static void main(String[] args) {
 
         Menu menuInfoMenu = new Menu();
-        Numbers setNumbers = new Numbers();
-        setNumbers.intNumbers();
-        menuInfoMenu.menuInfo();
-    }
-        
-        public void sum(int num1, int num2) {
-            int resultSum = num1 + num2;
-            System.out.println("resultSum: " + resultSum);
-        }
-
-        public void subtraction(int num1, int num2) {
-            int resultSubtraction = num1 - num2;
-            System.out.println("resultSubtraction: " + resultSubtraction);
-        }
-
-        public void multiplication(int num1, int num2) {
-            int resultMultiplication = num1 * num2;
-            System.out.println("resultMultiplication: " + resultMultiplication);
-        }
-
-        public void division(int num1, int num2) {
-            float resultDivision = num1 / num2;
-            System.out.println("resultDivision: " + resultDivision);
-        }
-    
-}
-
-class Menu{
-    public static void menuInfo() {
 
         Scanner variable = new Scanner(System.in);
-        Calculator calculator = new Calculator();
-        int number1, number2;
+
+        int num1, num2;
+
+        System.out.println("Welcome to the calculator programmed in java then enter 2 numbers");
+
+        System.out.println("First number:");
+        num1 = variable.nextInt();
+
+        System.out.println("Second number:");
+        num2 = variable.nextInt();
+
+        menuInfoMenu.menuInfo(num1, num2);
+    }
+}
+
+class Menu {
+    public void menuInfo(int number1, int number2) {
+
+        Scanner variable = new Scanner(System.in);
+        Functions function = new Functions();
 
         System.out.println("Enter the option you want to perform: ");
         System.out.println("1- Sum");
@@ -48,41 +34,50 @@ class Menu{
         System.out.println("4- Division");
         System.out.println("5- Exit");
 
-        int operacion = variable.nextInt();
+        int opt = variable.nextInt();
 
-        switch(operacion){
+        switch (opt) {
             case 1:
-                calculator.sum(number1, number2);
+                function.sum(number1, number2);
                 break;
 
             case 2:
-                calculator.subtraction(number1, number2);
+                function.subtraction(number1, number2);
                 break;
 
             case 3:
-                calculator.multiplication(number1, number2);
+                function.multiplication(number1, number2);
                 break;
-            
+
             case 4:
-                calculator.division(number1, number2);
+                function.division(number1, number2);
                 break;
 
             case 5:
-                break;}
+                break;
+        }
     }
 }
 
-class Numbers{
-    public static void intNumbers() {
-        Scanner variable = new Scanner(System.in);
-        int num1, num2;
+class Functions{
 
-        System.out.println("Welcome to the calculator programmed in java then enter 2 numbers");
-            
-        System.out.println("First number:");
-        num1 = variable.nextInt();
+    public void sum(int num1, int num2) {
+        int resultSum = num1 + num2;
+        System.out.println("resultSum: " + resultSum);
+    }
 
-        System.out.println("Second number:");
-        num2 = variable.nextInt();
+    public void subtraction(int num1, int num2) {
+        int resultSubtraction = num1 - num2;
+        System.out.println("resultSubtraction: " + resultSubtraction);
+    }
+
+    public void multiplication(int num1, int num2) {
+        int resultMultiplication = num1 * num2;
+        System.out.println("resultMultiplication: " + resultMultiplication);
+    }
+
+    public void division(float num1, float num2) {
+        float resultDivision = num1 / num2;
+        System.out.println("resultDivision: " + resultDivision);
     }
 }
