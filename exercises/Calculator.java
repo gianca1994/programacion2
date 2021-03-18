8d54e85e91ef5b2ff212f82a36095cd3ffe24515
gianca1994 <gianca9405@gmail.com>
2021-03-17 21:02:26 -0600

    Update

--- exercises/Calculator.java
+++ exercises/Calculator.java
@@ -1,20 +1,45 @@
 import java.util.Scanner;
 
-class Calculator{
+import jdk.dynalink.beans.StaticClass;
+
+class Calculator {
 
     public static void main(String[] args) {
 
+        Menu menuInfoMenu = new Menu();
+        Numbers setNumbers = new Numbers();
+        setNumbers.intNumbers();
+        menuInfoMenu.menuInfo();
+    }
+        
+        public void sum(int num1, int num2) {
+            int resultSum = num1 + num2;
+            System.out.println("resultSum: " + resultSum);
+        }
+
+        public void subtraction(int num1, int num2) {
+            int resultSubtraction = num1 - num2;
+            System.out.println("resultSubtraction: " + resultSubtraction);
+        }
+
+        public void multiplication(int num1, int num2) {
+            int resultMultiplication = num1 * num2;
+            System.out.println("resultMultiplication: " + resultMultiplication);
+        }
+
+        public void division(int num1, int num2) {
+            float resultDivision = num1 / num2;
+            System.out.println("resultDivision: " + resultDivision);
+        }
+    
+}
+
+class Menu{
+    public static void menuInfo() {
+
         Scanner variable = new Scanner(System.in);
-
-        int num1, num2;
-
-        System.out.println("Welcome to the calculator programmed in java then enter 2 numbers");
-            
-        System.out.println("First number:");
-        num1 = variable.nextInt();
-
-        System.out.println("Second number:");
-        num2 = variable.nextInt();
+        Calculator calculator = new Calculator();
+        int number1, number2;
 
         System.out.println("Enter the option you want to perform: ");
         System.out.println("1- Sum");
@@ -27,42 +52,37 @@ class Calculator{
 
         switch(operacion){
             case 1:
-                sum(num1, num2);
+                calculator.sum(number1, number2);
                 break;
 
             case 2:
-                subtraction(num1, num2);
+                calculator.subtraction(number1, number2);
                 break;
 
             case 3:
-                multiplication(num1, num2);
+                calculator.multiplication(number1, number2);
                 break;
             
             case 4:
-                division(num1, num2);
+                calculator.division(number1, number2);
                 break;
 
             case 5:
-                break;}}
+                break;}
+    }
+}
 
-        private static void sum(int num1, int num2) {
-            int resultSum = num1 + num2;
-            System.out.println("resultSum: " + resultSum);
-        }
+class Numbers{
+    public static void intNumbers() {
+        Scanner variable = new Scanner(System.in);
+        int num1, num2;
 
-        private static void subtraction(int num1, int num2) {
-            int resultSubtraction = num1 - num2;
-            System.out.println("resultSubtraction: " + resultSubtraction);
-        }
+        System.out.println("Welcome to the calculator programmed in java then enter 2 numbers");
+            
+        System.out.println("First number:");
+        num1 = variable.nextInt();
 
-        private static void multiplication(int num1, int num2) {
-            int resultMultiplication = num1 * num2;
-            System.out.println("resultMultiplication: " + resultMultiplication);
-        }
-
-        private static void division(int num1, int num2) {
-            float resultDivision = num1 / num2;
-            System.out.println("resultDivision: " + resultDivision);
-        }
-    
+        System.out.println("Second number:");
+        num2 = variable.nextInt();
+    }
 }
